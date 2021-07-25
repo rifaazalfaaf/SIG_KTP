11964744b96866f14c652590a055a3f4f53ccda7
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2021-07-25 12:23:24 +0700

    menghapus route yg tidak dipakai

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','HomeController@index');
 Route::get('/visualisasi_data','VisualisasiController@index');
 Route::get('/prediksi_data','PrediksiController@index');
-// Route::get('/input_data','PrediksiController@input_data');
 Route::post('/import', 'PrediksiController@import')->name('import');
 Route::post('/prediksi', 'PrediksiController@prediksi');
